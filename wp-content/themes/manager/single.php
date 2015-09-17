b15fdea4e2617c87994753becc1b27dd165ee295
Carlos Oliveira <carlos.baptista@b2wdigital.com>
2015-09-17 12:34:30 -0300

    feat(single): Update single view

--- wp-content/themes/manager/single.php
+++ wp-content/themes/manager/single.php
@@ -1,19 +1,24 @@
 <?php
   header('Content-Type: application/json; charset=utf-8');
 
-  if (have_posts()) : the_post();
-    $title   = utf8_encode(get_the_title());
-    $content = utf8_encode(get_the_content());
-    $url     = parse_url(get_permalink())['path'];
+  if (have_posts()) {
+    $title      = utf8_encode(get_the_title());
+    $content    = utf8_encode(get_the_content());
+    $url        = parse_url(get_permalink())['path'];
+    $template   = get_post_meta(get_the_ID(), 'template', true);
+
+    //$hasTemplate = $_GET['template'];
 
     $data = array(
-      'title'   => $title,
-      'content' => $content,
-      'url'     =>  $url
+      'title'     => $title,
+      'content'   => $content,
+      'url'       =>  $url,
+      'template'  => $template
+      //'temp' => $hasTemplate
     );
 
     echo json_encode($data, JSON_UNESCAPED_UNICODE);
-  else :
+  } else {
     echo 'Departamento não encontrado';
-  endif;
+  }
 ?>
